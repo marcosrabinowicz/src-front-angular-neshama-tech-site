e1d9f09a73c8b511ff80ed8eb52395aea47f06b6
Marcos Rabinowicz <marcosrabinowicz@hotmail.com>
2025-07-31 19:46:03 -0300

    refactor: separa estilos por componente e limpa app.component.scss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,25 @@
 import { Component, signal } from '@angular/core';
+import { Home } from './sections/home/home';
+import { Sobre } from './sections/sobre/sobre';
+import { Servicos } from './sections/servicos/servicos';
+import { Portfolio } from './sections/portfolio/portfolio';
+import { Contato } from './sections/contato/contato';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  imports: [Home, Sobre, Servicos, Portfolio, Contato],
 })
 export class App {
   protected readonly title = signal('neshama-tech-site');
+  protected menuAberto = false;
+
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
+
+  fecharMenu() {
+    this.menuAberto = false;
+  }
 }
